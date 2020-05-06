2cc3388bbc8a8bf46a9c81fae6c5578e2b776eca
hbrueckler <hbrueckler@uos.de>
2020-05-06 15:23:42 +0200

    Remove alternate parallelization methods

--- include/crash2mesh/util/par_for.hpp
+++ include/crash2mesh/util/par_for.hpp
@@ -5,15 +5,7 @@
 
 #include <thread>
 #if defined(C2M_PARALLEL)
-#if 0 //defined(__cpp_lib_parallel_algorithm)
-#include <algorithm>
-#include <execution>
-#elif 0
-#include <future>
-#include <atomic>
-#else
 #include <sandbox/ThreadPool/ThreadPool.h>
-#endif
 #endif
 
 namespace c2m
@@ -22,23 +14,6 @@ namespace c2m
 template<class T, class C>
 void parallel_for_each(std::vector<T>& ts, C callable, uint poolsize = std::thread::hardware_concurrency()) {
 #if defined(C2M_PARALLEL)
-#if 0 //defined(__cpp_lib_parallel_algorithm)
-    std::for_each(std::execution::par_unseq, ts.begin(), ts.end(), callable);
-#elif 0
-    std::atomic<size_t> index(0);
-    std::vector<std::thread> threads;
-
-    for (uint i = 0; i < poolsize; i++) {
-        threads.emplace_back([&]() {
-            while(index < ts.size()) {
-                callable(ts[index++]);
-            }
-        });
-    }
-    for (auto&& thread : threads) {
-        thread.join();
-    }
-#else
     ThreadPool pool(poolsize);
     std::vector<std::future<typename std::result_of<C(T)>>> results;
     for (auto && t: ts)
@@ -46,7 +21,6 @@ void parallel_for_each(std::vector<T>& ts, C callable, uint poolsize = std::thre
 
     for (auto &&result: results)
         result.wait();
-#endif
 #else
     (void)poolsize;
     for (auto && t: ts)
